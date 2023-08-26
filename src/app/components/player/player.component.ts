b20745a1689d9be36f6f7ced8bdbaa6f2852eabe
Josh <josh_alot@protonmail.com>
2023-08-25 22:45:56 -0300

    changing the play and pause button when click

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.scss']
 })
-export class PlayerComponent {
-  
+export class PlayerComponent implements OnInit {
+  isSongPlaying?: boolean;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.isSongPlaying = false;
+  }
+
+  changePlayIcon() {
+    this.isSongPlaying = !this.isSongPlaying;
+  }
 }
